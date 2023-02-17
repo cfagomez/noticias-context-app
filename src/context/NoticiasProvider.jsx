3c3951941d0290892640139861af5fe0c835df93
Federico <cuervog992@gmail.com>
2023-02-16 22:15:22 -0300

    conexion entre estado categoria y evento onChange de select

--- src/context/NoticiasProvider.jsx
+++ src/context/NoticiasProvider.jsx
@@ -1,10 +1,29 @@
-import { createContext } from "react"
+import { createContext, useEffect, useState } from "react"
 
 const NoticiasContext = createContext()
 
 const NoticiasProvider = ({children}) => {
+
+  const [categoria, setCategoria] = useState('general')
+
+  useEffect(() => {
+
+    const consultarAPI = async () => {
+
+      const url = `https://newsapi.org/v2/top-headlines?country=ar&category=${categoria}&apikey=${import.meta.env.VITE_API_KEY}`
+
+    }
+
+  })
+
+  const handleChangeCategoria = (e) => {
+
+    setCategoria(e.target.value)
+
+  }
+
   return (
-    <NoticiasContext.Provider value={{}}>
+    <NoticiasContext.Provider value={{categoria, handleChangeCategoria}}>
         {children}
     </NoticiasContext.Provider>
   )
